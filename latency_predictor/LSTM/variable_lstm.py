54d2fc967bcf19592795ab50ae1e9ecebc59fd82
Lloneffy <longfeilu@std.uestc.edu.cn>
2020-11-06 23:12:17 +0800

    with lstm and var_lstm

--- latency_predictor/LSTM/variable_lstm.py
+++ latency_predictor/LSTM/variable_lstm.py
@@ -1,4 +1,5 @@
 from torch import nn
+
 # Define LSTM Neural Networks
 class LstmRNN(nn.Module):
     """
@@ -15,33 +16,13 @@ class LstmRNN(nn.Module):
         self.lstm = nn.LSTM(input_size, hidden_size, num_layers)  # utilize the LSTM model in torch.nn   1, 16, 1, 1
         self.forwardCalculation = nn.Linear(hidden_size*20, output_size)
 
-    # def forward(self, _x, d_list):
-    #     self.d_list = d_list
-    #     x, _ = self.lstm(_x)  # _x is input, size (seq_len, batch, input_size)
-    #     x=x.clone()
-    #     s, b, h = x.shape  # x is output, size (seq_len, batch, hidden_size)  [20,128,16]
-    #     for k, d_ls in enumerate(d_list):
-    #         d_ls = d_ls.numpy().tolist()
-    #         start = 0
-    #         end = 4
-    #         for depth in d_ls:
-    #             for j in range(start + depth, end):
-    #                 x[j, k, :] = 0
-    #             start += 4
-    #             end += 4
-    #     # print(x)
-    #     x = x.view(b, s * h)  # [128,16*20]
-    #     x = self.forwardCalculation(x)  # [128*20,1]
-    #     # x = x.view(s, b, -1)  # [20,128,1]
-    #     return x
     def forward(self, _x, d_list):
         self.d_list = d_list
         x, _ = self.lstm(_x)  # _x is input, size (seq_len, batch, input_size)
-
+        x = nn.utils.rnn.pad_packed_sequence(x,batch_first=True)
         s, b, h = x.shape  # x is output, size (seq_len, batch, hidden_size)  [20,128,16]
         x = x.view(b, s * h)  # [128,16*20]
         x = self.forwardCalculation(x)  # [128*20,1]
         # x = x.view(s, b, -1)  # [20,128,1]
         return x
-
 
--- latency_predictor/Net/data_process.py
+++ latency_predictor/Net/data_process.py
@@ -3,6 +3,9 @@ import torch
 import copy
 import os
 import random
+
+from torch.utils.data import TensorDataset, DataLoader
+
 os.environ["CUDA_VISIBLE_DEVICES"] = '0, 1, 2, 3'
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
@@ -26,6 +29,7 @@ class Dataprocess():
     def encode(self):
         all_feats = []
         gpu, cpu =[], []
+        d_lists = []
         df = pd.read_csv(self.file_path, sep='\t')
         population = df.values
         random.shuffle(population)
@@ -35,18 +39,19 @@ class Dataprocess():
             ks_list = copy.deepcopy(para['ks'])
             ex_list = copy.deepcopy(para['e'])
             d_list = copy.deepcopy(para['d'])
+            d_lists.append(d_list)
             r = copy.deepcopy(para['r'])[0]
-            feats = self.spec2feats(ks_list, ex_list, d_list, r).reshape(1, -1)
+            feats= self.spec2feats_var_lstm(ks_list, ex_list, d_list, r)
             gpu.append(sample[1])
             cpu.append(sample[2])
             all_feats.append(feats)
             # print(all_feats)
         all_feats = torch.cat(all_feats, 0) #row
         # pred = self.model(all_feats).cpu()
-        return all_feats, gpu, cpu
+        return all_feats, gpu, cpu,d_lists
 
 
-    # @staticmethod
+    @staticmethod
     def spec2feats(self, ks_list, ex_list, d_list, r):
         # This function converts a network config to a feature vector (128-D).
         start = 0
@@ -74,6 +79,52 @@ class Dataprocess():
         return torch.Tensor(ks_onehot + ex_onehot + r_onehot)
 
     @staticmethod
+    def spec2feats_lstm(ks_list, ex_list, d_list, r):
+        tmp = []
+        for ks, ex in zip(ks_list, ex_list):
+            tmp.append(torch.tensor([ks, ex, r/100]))
+        ten = torch.cat(tmp, -1)
+        ten = torch.unsqueeze(ten,dim=0).view(-1, 20, 3)
+        return ten
+
+    @staticmethod
+    def spec2feats_lstm_with_0(ks_list, ex_list, d_list, r):
+        start = 0
+        end = 4
+        for d in d_list:
+            for j in range(start + d, end):
+                ks_list[j] = 0
+                ex_list[j] = 0
+            start += 4
+            end += 4
+        tmp = []
+        for ks, ex in zip(ks_list, ex_list):
+            tmp.append(torch.tensor([ks, ex, r / 100]))
+        ten = torch.cat(tmp, -1)
+        ten = torch.unsqueeze(ten, dim=0).view(-1, 20, 3)
+        return ten
+
+    @staticmethod
+    def spec2feats_var_lstm(ks_list, ex_list, d_list, r):
+        tmp = []
+        start = 0
+        end = 4
+        for d in d_list:
+            for j in range(start + d, end):
+                ks_list[j] = 0
+                ex_list[j] = 0
+            start += 4
+            end += 4
+
+        for ks, ex in zip(ks_list, ex_list):
+            if ks != 0 and ex!= 0:
+                tmp.append(torch.tensor([ks, ex, r / 100]))
+        ten = torch.cat(tmp, -1)
+        ten = torch.unsqueeze(ten, dim=0).view(-1, 20, 3)
+        return ten
+
+
+    @staticmethod
     def spec2feats_v2(ks_list, ex_list, d_list, r):
         # This function converts a network config to a feature vector (128-D).
         start = 0
@@ -98,6 +149,67 @@ class Dataprocess():
 
         r_onehot[(r - 112) // 16] = 1
         return torch.Tensor(ks_onehot + r_onehot)
+
+def dataloader(ratio, batch_size, file_path):
+    dataprocess = Dataprocess(file_path)
+    all_feats, gpu, cpu, d_list= dataprocess.encode()
+    totalnum = len(cpu)
+    cpu = torch.tensor(cpu).reshape(totalnum, -1)
+    gpu = torch.tensor(gpu).reshape(totalnum, -1)
+    d_list =torch.tensor(d_list).reshape(totalnum, -1)
+    # feature = all_feats[:int(0.8*totalnum)]
+    train_dataset = TensorDataset(all_feats[:int(ratio[0] * totalnum)], cpu[:int(ratio[0] * totalnum)],d_list[:int(ratio[0] * totalnum)])
+    valid_dataset = TensorDataset(all_feats[int(ratio[0] * totalnum):int((ratio[1]+ratio[0]) * totalnum)],
+                                  cpu[int(ratio[0] * totalnum):int((ratio[1]+ratio[0]) * totalnum)],
+                                  d_list[int(ratio[0] * totalnum):int((ratio[1]+ratio[0]) * totalnum)])
+    test_dataset =  TensorDataset(all_feats[int((ratio[1]+ratio[0]) * totalnum):],cpu[int((ratio[1]+ratio[0]) * totalnum):],
+                                  d_list[int((ratio[1]+ratio[0]) * totalnum):])
+
+    train_loader = DataLoader(dataset=train_dataset, batch_size=batch_size, shuffle=True,drop_last=True)
+    valid_loader = DataLoader(dataset=valid_dataset, batch_size=batch_size, shuffle=True, drop_last=True)
+    test_loader = DataLoader(dataset=test_dataset, batch_size=batch_size,shuffle=True, drop_last=True)
+    return train_loader, valid_loader, test_loader
+
+class var_lstm_data(torch.utils.data.Dataset):
+    def __init__(self, data_seq, data_label,d_list):
+        self.data_seq = data_seq
+        self.data_label = data_label
+        self.d_list = d_list
+    def __len__(self):
+        return len(self.data_seq)
+    def __getitem__(self, idx):
+        return self.data_seq[idx], self.data_label[idx]
+
+    def collate(data):
+        data.sort(key=lambda x:len(x[0]), reverse=True)
+        data_length = [len(sq[0]) for sq in data]
+        x = [i[0] for i in data]
+        y = [i[1] for i in data]
+        data = torch.nn.utils.rnn.pad_sequence(x,batch_first=True,padding_value=0)
+        return data.unsqueeze(-1), data_length, torch.tensor(y, dtype=torch.float32)
+
+def var_lstm_dataloader(ratio, batch_size, file_path):
+    dataprocess = Dataprocess(file_path)
+    all_feats, gpu, cpu, d_list= dataprocess.encode()
+    totalnum = len(cpu)
+    cpu = torch.tensor(cpu).reshape(totalnum, -1)
+    gpu = torch.tensor(gpu).reshape(totalnum, -1)
+    d_list =torch.tensor(d_list).reshape(totalnum, -1)
+    # feature = all_feats[:int(0.8*totalnum)]
+
+    train_dataset = var_lstm_data(all_feats[:int(ratio[0] * totalnum)], cpu[:int(ratio[0] * totalnum)],d_list[:int(ratio[0] * totalnum)])
+    valid_dataset = var_lstm_data(all_feats[int(ratio[0] * totalnum):int((ratio[1]+ratio[0]) * totalnum)],
+                                  cpu[int(ratio[0] * totalnum):int((ratio[1]+ratio[0]) * totalnum)],
+                                  d_list[:int(ratio[0] * totalnum)])
+    test_dataset =  var_lstm_data(all_feats[int((ratio[1]+ratio[0]) * totalnum):],cpu[int((ratio[1]+ratio[0]) * totalnum):],
+                                  d_list[:int(ratio[0] * totalnum)])
+
+    train_loader = DataLoader(dataset=train_dataset, batch_size=batch_size, shuffle=True,drop_last=True)
+    valid_loader = DataLoader(dataset=valid_dataset, batch_size=batch_size, shuffle=True, drop_last=True)
+    test_loader = DataLoader(dataset=test_dataset, batch_size=batch_size,shuffle=True, drop_last=True)
+    return train_loader, valid_loader, test_loader
+
+
 
 
 
